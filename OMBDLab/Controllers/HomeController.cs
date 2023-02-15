d184bcc01f1aa5d93ba72f400e25d1e3b389fff0
camdixon132 <camdixon132@gmail.com>
2023-02-15 08:33:05 -0500

    First commit

---
+++ OMBDLab/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using OMBDLab.Models;
+
+namespace OMBDLab.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult MovieSearch()
+    {
+        return View();
+    }
+
+    /*
+    [HttpGet]
+    public IActionResult MovieSearchForm()
+    {
+
+        return View(MovieSearch);
+    }
+    */
+    [HttpPost]
+    public IActionResult MovieSearch(string movie)
+    {
+
+        return View(MovieDAL.MovieSearchResults(movie));
+    }
+
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ OMBDLab/Models/MovieDAL.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace OMBDLab.Models
+{
+	public class MovieDAL
+	{
+            public static MovieModel MovieSearchResults(string movie)//adjust here
+            {
+                //Adjust here
+                //Setup
+                string url = $"https://www.omdbapi.com/?t={movie}&apikey=737c6f71";
+
+                //request
+                HttpWebRequest request = WebRequest.CreateHttp(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                //Convert it to JSON
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                string JSON = reader.ReadToEnd();
+
+                //Adjust here
+                //Convert to C#
+                MovieModel result = JsonConvert.DeserializeObject<MovieModel>(JSON);
+                return result;
+            }
+    }
+}
+
